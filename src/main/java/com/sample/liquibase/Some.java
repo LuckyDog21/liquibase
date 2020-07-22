fecfc0a1a909d54b1cb2029b9c9cbf6eeef0f4ca
alex <voltov2017@yandex.ru>
2020-07-22 11:36:15 +0300

    first commit

---
+++ src/main/java/com/sample/liquibase/Some.java
@@ -0,0 +1,12 @@
+package com.sample.liquibase;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/*@Table(name = "some")
+@Entity
+public class Some {
+    @Id
+    private Long body;
+}*/
